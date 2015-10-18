43eea82c007f4b5895791dae06be4c2e0653a229
Corey Farris <cfarris@protonmail.ch>
2015-10-17 23:32:28 -0400

    Update mortal_rabbits function in rabbits.jl

--- rabbits.jl
+++ rabbits.jl
@@ -1,12 +1,54 @@
-function rabbits(n::Integer, k::Integer)
-  m1 = 1
-  m2 = 1
-  println(m1)
-  println(m2)
-  for i in 3:n
-    temp = m1
-    m1 = m2
-    m2 += temp*k
-    println(m2)
+function rabbits(no_of_months::Integer, no_per_litter::Integer)
+  previous_count = 1
+  current_count = 1
+
+  if no_of_months < 3
+    if no_of_months >= 1
+      return 1
+    else
+      return 0
+    end
   end
+
+  for i in 3:no_of_months
+    temp = previous_count
+    previous_count = current_count
+    current_count += temp*no_per_litter
+  end
+
+  return current_count
+end
+
+# can't use the rabbits function because it doesn't accurately show the count
+# with a finite lifetime, have to recursively call itself must be rewritten
+function mortal_rabbits(no_of_months::Integer, lifespan_in_months::Integer)
+  previous_count = 1
+  current_count = 1
+
+  if no_of_months < 3
+    if no_of_months >= 1
+      return 1
+    else
+      return 0
+    end
+  end
+
+  count_history = Array{Integer}(no_of_months)
+  count_history[1] = 1
+  count_history[2] = 1
+
+  for i in 3:no_of_months
+    temp = previous_count
+    previous_count = current_count
+    if i - lifespan_in_months < 1
+      current_count += temp
+      count_history[i] = current_count
+    else
+      println(current_count, " - ", count_history[i - lifespan_in_months])
+      current_count += temp - count_history[i - lifespan_in_months]
+      count_history[i] = current_count
+    end
+  end
+
+  return current_count
 end
