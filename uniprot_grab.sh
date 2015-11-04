170f42801f2fc4fcafde30c3317b9e7f8bfec6a4
Corey Farris <cfarris@protonmail.ch>
2015-11-04 12:51:44 -0500

    Modify uniprot_grab script to allow individual arguments

--- uniprot_grab.sh
+++ uniprot_grab.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-grep . $1 | while read -r line ; do
-  curl -OL "http://www.uniprot.org/uniprot/$line.fasta"
-done
+if [ -f "$1"] ; then
+  grep . $1 | while read -r line ; do
+    curl -OL "http://www.uniprot.org/uniprot/$line.fasta"
+  done
+else
+  curl -OL "http://www.uniprot.org/uniprot/$1.fasta"
+fi
